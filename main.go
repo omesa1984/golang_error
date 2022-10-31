de388ffedeadfca09312478310b8230e63ad8876
omesa198 <omesa1984@gmail.com>
2022-10-31 14:46:25 -0300

    Usando 'log' para o tratamento de error

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	_, err := os.Open("no-file.txt")
 	if err != nil {
-		fmt.Println("err happened", err)
-		//		log.Println("err happened",err)
+		//fmt.Println("err happened", err)
+		log.Println("err happened", err)
 		//		log.Fatalln(err)
 		//		panic(err)
 	}
 }
 
-//Println formats using the daefault formats for its operands and writes to standard output
+/*
+Package log implements a simple logging package
+writes to standard error and prints the date and
+time of each logged message
+*/
+
+/*
+log.Println calls Output to print to the standard
+logger. Arguments are handled in the manner of
+fmt.Println.
+*/
