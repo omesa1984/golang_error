a0c0d80b7cd4d04775e0a4415ead9fc1155ae597
omesa198 <omesa1984@gmail.com>
2022-10-31 17:27:01 -0300

    Error com informação adicional - 05. Declaração do Error como 'struct'

--- main.go
+++ main.go
@@ -6,19 +6,40 @@ import (
 	"math"
 )
 
+type norgateMathError struct {
+	menor0 bool
+	err    error
+}
+
+func (n norgateMathError) Error() string {
+	return fmt.Sprintf("a norgate math error ocurred: \n O núero é < 0:  %v \n %v", n.menor0, n.err)
+}
+
 func main() {
-	var f float64 = -10
+	var f float64 = 10
+
 	v, err := sqrt(f)
 	if err != nil {
-		log.Fatalln(err)
+		n := norgateMathError{false, nil}
+		MenorMaior(f, n)
+		log.Println(err)
 	} else {
 		fmt.Println("O valor da raiz de: ", f, " é ", v)
 	}
 }
 
+func MenorMaior(f float64, n norgateMathError) {
+	if f < 0 {
+		n.menor0 = true
+	} else {
+		n.menor0 = false
+	}
+}
+
 func sqrt(f float64) (float64, error) {
 	if f < 0 {
-		return 0, fmt.Errorf("norgate math: square root of negative number: %v", f)
+		nme := fmt.Errorf("norgate math redux: square root of negative number: %v", f)
+		return 0, norgateMathError{true, nme}
 	}
 	return math.Sqrt(f), nil
 }
